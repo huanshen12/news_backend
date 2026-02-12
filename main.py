a53f399bd51303741ac31bcae52d8f255cab5f1d
huanshen12 <157880036+huanshen12@users.noreply.github.com>
2026-02-12 20:50:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
-from routers import news
+from routers import news, users
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# 配置CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许所有来源，你可以根据需要修改为具体的域名
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+   
 
 @app.get("/")
 async def root():
@@ -14,3 +25,4 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 app.include_router(news.router)
+app.include_router(users.router)
